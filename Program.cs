8daf189f2e1b3af2906a7d5f9a43b406e6eb1cb1
Muath Mhawich <23041194@student.ltuc.com>
2024-08-19 13:26:49 +0300

    solve lab 14

--- Program.cs
+++ Program.cs
@@ -18,14 +18,43 @@ builder.Services.AddControllers()
 string ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
 
 builder.Services.AddDbContext<TunifyDbContext>(optionsX => optionsX.UseSqlServer(ConnectionString));
+
+
+
+
 //builder.Services.AddTransient<IEmployee, EmployeeService>();
 builder.Services.AddScoped<Iuser, UserServices>();
 builder.Services.AddScoped<ISong, SongServices>();
 builder.Services.AddScoped<Iplaylist, PlayListServices>();
 builder.Services.AddScoped<Iartist, ArtistServices>();
-
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Tunify API",
+        Version = "v1",
+        Description = "API for managing playlists, songs, and artists in the Tunify Platform"
+    });
+});
 var app = builder.Build();
+
+app.UseSwagger(
+             options =>
+             {
+                 options.RouteTemplate = "api/{documentName}/swagger.json";
+             }
+);
+
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/api/v1/swagger.json", "Tunify API v1");
+    options.RoutePrefix = "";
+});
 app.MapControllers();
+
+
+
 
 app.MapGet("/", () => "Hello World!");
 
